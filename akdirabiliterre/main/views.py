adef92a4cfa57b99c28e3ac93ea679d5aa5d615f
Eklou Amemassovor <amekley@gmail.com>
2018-07-25 03:56:37 +0000

    next start

--- akdirabiliterre/main/views.py
+++ akdirabiliterre/main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from zoom.models import Zoom
+from blog.models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    zooms=Zoom.objects.filter(main=True)[:3]
+    
+    context={
+        'posts':Post.objects.all()[:3],
+        'zoom1':zooms[0]
+
+    }
+    return render(request, 'main/index.html',context)
